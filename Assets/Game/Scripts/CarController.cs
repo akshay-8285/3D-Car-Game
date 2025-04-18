62a8d08bdc8dfdeba90a7ba0eb136482c1a0324c
akshay-8285 <akshaysagar585@gmail.com>
2025-04-18 23:58:29 +0530

    Updated

--- Assets/Game/Scripts/CarController.cs
+++ Assets/Game/Scripts/CarController.cs
@@ -1,16 +1,105 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum Axel
     {
-        
+        Front,
+        Rear
     }
 
-    // Update is called once per frame
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public float maxAcceleration = 30.0f;
+    public float breakAcceleration = 50.0f;
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 50.0f;
+    public List<Wheel> wheels = new List<Wheel>();
+    float moveInput;
+    float turnInput;
+
+    private Rigidbody carRb;
+    private Vector3 centerOfMass;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = centerOfMass;
+    }
+
     void Update()
     {
-        
+        GetInputs();
+        AnimateWheels();
+    }
+
+    void LateUpdate()
+    {
+        Move();
+        Steer();
+        Brake();
+       
+    }
+
+    public void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    public void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput  * 600 * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    public void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var _steerAngle = turnInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle,  0.6f);
+            }
+        }
+    }
+
+    public void Brake()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = breakAcceleration   * 300 * Time.deltaTime;
+            }
+        }
+        else
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
+        }
+    }
+
+    public void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.GetWorldPose(out Vector3 position, out Quaternion rotation);
+            wheel.wheelModel.transform.position = position;
+            wheel.wheelModel.transform.rotation = rotation;
+        }
     }
 }
--- Assets/Script/CameraFollowScript.cs
+++ Assets/Script/CameraFollowScript.cs
@@ -4,36 +4,30 @@ using UnityEngine;
 
 public class CameraFollowScript : MonoBehaviour
 {
-    //public Transform target; // Player car ka reference
+    [SerializeField] private Transform car;
+    public Vector3 moveOffset;
+    public Vector3 rotationOffset;
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 5f;
 
-    //public Vector3 offset; // Camera ka position offset
-    //public float smoothSpeed = 0.125f; // Camera ka smooth transition speed
-
-    //void LateUpdate()
-    //{
-    //    if (target != null)
-    //    {
-    //        Vector3 desiredPosition = target.position + offset; // Player car ke position par camera ko set karein
-    //        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // Smoothly transition karein
-    //        transform.position = smoothedPosition; // Update the camera position
-    //    }
-    //}
-
-    public Transform car; // Car GameObject ka reference
-    public float cameraDistance = 2f; // Camera ka distance car se
-    public float cameraHeight = 1f; // Camera ki height car se
-    public float cameraFollowSpeed = 5f; // Camera ka follow speed
-
-    void LateUpdate()
+    public void FixedUpdate()
     {
-        if (car != null)
-        {
-            // Car ke position ko follow karna
-            Vector3 targetPosition = car.position - car.forward * cameraDistance + Vector3.up * cameraHeight;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraFollowSpeed);
+      HandleMovement();
+      HandleRotation();
 
-            // Car ke direction mein camera ko rotate karna
-            transform.LookAt(car);
-        }
+    }
+
+    public void HandleMovement()
+    {
+        Vector3 targetPos = new Vector3();
+        targetPos = car.TransformPoint(moveOffset);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * movementSpeed);
+    }
+    public void HandleRotation()
+    {
+        var direction  = car.position - transform.position;
+        var rotation = new Quaternion();
+        rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
     }
 }
